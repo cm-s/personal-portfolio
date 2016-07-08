4c6e3f6645e2cb5dad9c9482f6c5738cdbd4fac3
cm-s <blufyr3530@gmail.com>
2016-07-07 20:40:59 -0700

    formatting upgrades + added jquery libs & sandbox

---
+++ personal-portfolio/Landing/script.js
@@ -0,0 +1,8 @@
+var main = function() {
+    $('#jsportal').click(function() {
+        console.log('done');
+        window.location = "../Javabox/javabox.html";
+    });
+};
+
+$(document).ready(main);
