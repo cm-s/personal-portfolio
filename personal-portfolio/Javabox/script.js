971a43aa6a80013469e151cb17d2b42cd201809f
cm-s <blufyr3530@gmail.com>
2016-07-09 21:17:17 -0700

    added font assets, + polished landing header

--- personal-portfolio/Javabox/script.js
+++ personal-portfolio/Javabox/script.js
@@ -1,3 +1,12 @@
+function textOperator() {
+    var greetingText = "Please enter your Username and Password";
+    /*for (var index = 0; index < 39; index++) {
+        console.log("This is bad")
+        $('#greeting').append(greetingText[index]);
+        sleep(5.0);
+    };*/
+    $('#greeting').append(greetingText);
+};
 $(document).ready(function() {
-    //code
-});
+    textOperator();
+})
--- personal-portfolio/Landing/script.js
+++ personal-portfolio/Landing/script.js
@@ -1,5 +1,5 @@
-var oldScroll = 0;
-var opacity = 0;
+var headerHasText = false;
+
 $(document).ready(function() {
     //setup button
     $('#jsportal').click(function() {
@@ -7,13 +7,26 @@ $(document).ready(function() {
     });
     var pHeight = $('.text-sect#one').height();
     $('#jsportal').css('margin-top', pHeight + 35 + 'px');
+    //manage header bar
     $(document).on('scroll', function() {
         $('#header-bar').css('background-color', 'rgba(117, 114, 114, ' + ((window.scrollY/3.5)/100) + ')');
         if ( (window.scrollY/3.5)/100 >= 1 ) {
             $('#header-bar').css('box-shadow', '0 5px 5px rgba(0, 0, 0, 0.26)');
+            if ( !headerHasText ) {
+                $('#header-bar-text').animate({'opacity': '1'}, 400);
+                headerHasText = true;
+            };
         };
         if ( (window.scrollY/3.5)/100 < 1 ) {
             $('#header-bar').css('box-shadow', 'initial');
+            if ( headerHasText ) {
+                $('#header-bar-text').animate({'opacity': '0'}, 300);
+                headerHasText = false;
+            };
         };
+    });
+    $('#button-ripple').click(function() {
+        $('#button-ripple').animate({'opacity': '0.5'}, 80).delay(30).animate({'opacity': '0'}, 80);
+        window.scroll(0, 0);
     });
 });
