5b15bf195a02bad2a049d8bacbace89c3e2aa365
cm-s <blufyr3530@gmail.com>
2016-10-18 21:59:44 -0700

    styling touchups + added nutrition facts

--- cms_portfolio/app/assets/javascripts/core/projects.js
+++ cms_portfolio/app/assets/javascripts/core/projects.js
@@ -3,6 +3,11 @@ var introText = "only"
 $(document).on('turbolinks:load', function() {
 if ($('body').is('#projects'))
 {
-
+    setTimeout(function () {
+        $('#pw-text-container').animate({'opacity': '0'}, 1900);
+        setTimeout(function () {
+            $('#pw-text-container').css('display', 'none');
+        }, 1901);
+    }, 3000);
 };
 });
